e4fcb923bd065d542d6c8c4f0c4e85b2da137dbd
Kanokon Netsiri <jaylor@Jaylors-MacBook-Air.local>
2024-04-10 21:04:05 +0700

    bp3-kanokon-netsiri

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,28 @@
 // Start coding here
+let add = (a, b) => {
+    return a + b;
+};
+
+let sum = add(10,5);
+
+
+let subtract = (a, b) => {
+    return a - b;
+};
+
+let total = subtract(10,5);
+
+
+let multiply = (a,b) => {
+    return a * b;
+};
+
+let sumOfMultiply = multiply(10,5);
+
+
+let divide = (a,b) => {
+    return a / b;
+};
+
+let sumOfDivide = divide(10,5);
+
